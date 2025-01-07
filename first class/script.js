2c5b7e6e30ff14ff257212737461160f32c1d434
Bitshifter-9 <bandarampranav@gmail.com>
2025-01-07 10:13:37 +0530

    first class

---
+++ first class/script.js
@@ -0,0 +1,16 @@
+alert("hi")
+console.log("Hello World!") // it will store on console in the browser
+document.write("Hello")  //it will display on page
+
+let x = 9
+console.log(x)
+x = 24
+console.log(x) // when you use let you can update the varible value
+
+const y = 99
+console.log(y)
+y = 19
+console.log(y)
+// it raises error const value will not change
+
+// CHECK DIFFERENCE BETWEEN LET,CONST,VAR
