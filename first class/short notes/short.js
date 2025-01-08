e66f0eb587dac7767ff3fa65521f786f1bec7faa
Bitshifter-9 <bandarampranav@gmail.com>
2025-01-08 10:55:33 +0530

    ds

--- first class/short notes/short.js
+++ first class/short notes/short.js
@@ -1,4 +1,4 @@
 /* 
 1. array.length (for length of the array)
 2.array.push() (for adding element to array at the last,like append in python for list)
-3. array.pop() (removes the last element from the array ; if you assing a varible to it , you can return it  )
+3. array.pop() (removes the last element from the array ; if you assing a varible to it , you can return it  )*/
