b821fd32f29f33683496df5a0e844c2c27361a07
Bitshifter-9 <bandarampranav@gmail.com>
2025-01-09 09:08:45 +0530

    add

--- first class/short notes/short.js
+++ first class/short notes/short.js
@@ -1,4 +1,34 @@
 /* 
 1. array.length (for length of the array)
-2.array.push() (for adding element to array at the last,like append in python for list)
-3. array.pop() (removes the last element from the array ; if you assing a varible to it , you can return it  )*/
+2.array.push() (for adding element to array at the last,like append in python for list;if you assign a variable to it you can return it)
+3. array.pop() (removes the last element from the array ; if you assing a varible to it , you can return it  )
+4. typeof (ex: typeof 25,output is number)
+5.alert() (gives allert on webpage)
+
+
+TYPES IN JA (CHECK IN GOOGLE)
+string and number will be same as python ; bool will be complete small letters(true,false);
+object={};
+symbol=symbol();
+check complex number and there are 8 types
+
+for loop in js, there are different types of for loop in js
+1.for (iterator; condition; iteration) {
+  logic;
+}
+2.for..of loop
+for (const value of iterable) {
+
+}
+compare with python 
+for i in range x:
+here value=i and x=iterable
+
+varable.repeat()
+
+creation of function
+function x(y){}
+x(z) (you should call the function like python)
+x is function,y is variable
+
+*/
