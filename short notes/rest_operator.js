86e1a2d0ae5e6c61bb1dc85dd04d1142158ea3fb
Bitshifter-9 <bandarampranav@gmail.com>
2025-03-18 22:31:21 +0530

    dcs

---
+++ short notes/rest_operator.js
@@ -0,0 +1,6 @@
+function x(a,b,...others){
+    console.log(others)
+    console.log(a,b)
+    console.log(others[0])
+}
+x(1,2,3,4,5,6,7,8,9,10) // [3,4,5,6,7,8,9,10] creates an array
---
+++ short notes/spread_operator.js
@@ -0,0 +1,8 @@
+a=[1,2,3]
+function x(a,b,c){
+    console.log(a,b,c)
+}
+x(a[0],a[1],a[2]) // 1 2 3
+x(...a) // 1 2 3
+x(...a)
+x(a)
